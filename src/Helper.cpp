723c5a812710179b1e0b075bd8c601b8c6774a30
Alexander Infantino <infantinoalex@gmail.com>
2018-05-02 15:30:18 -0400

    Chaning the static image objects to be a little more detailed

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -16,23 +16,37 @@
 static std::map<Colors, Image> CreateShapeImages()
 {
     std::map<Colors, Image> colorMap = std::map<Colors, Image>();
+    int width = 10;
+    int height = 24;
+
 
     unsigned int squareImage[] = 
     {
         0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,
 	0,1,1,1,1,1,1,1,1,0,
+        0,1,1,1,1,1,1,1,1,0,
+        0,1,1,1,1,1,1,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
+	0,1,1,0,0,0,0,1,1,0,
 	0,1,1,0,0,0,0,1,1,0,
 	0,1,1,0,0,0,0,1,1,0,
 	0,1,1,0,0,0,0,1,1,0,
 	0,1,1,0,0,0,0,1,1,0,
 	0,1,1,1,1,1,1,1,1,0,
+	0,1,1,1,1,1,1,1,1,0,
+	0,1,1,1,1,1,1,1,1,0,
 	0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0
     };
-
-    int width = 10;
-    int height = 10;
 
     std::vector<unsigned int> squareImageVector(squareImage, squareImage + sizeof(squareImage) / sizeof(squareImage[0]));
     Image BlueSquare = Image();
@@ -42,22 +56,102 @@ static std::map<Colors, Image> CreateShapeImages()
     {
         0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
 	0,0,0,1,1,1,1,0,0,0,
+	0,0,0,1,1,1,1,0,0,0,
+	0,0,0,1,1,1,1,0,0,0,
+	0,0,1,0,0,0,0,1,0,0,
+	0,0,1,0,0,0,0,1,0,0,
 	0,0,1,0,0,0,0,1,0,0,
 	0,1,0,0,0,0,0,0,1,0,
 	0,1,0,0,0,0,0,0,1,0,
+	0,1,0,0,0,0,0,0,1,0,
+	0,1,0,0,0,0,0,0,1,0,
+	0,1,0,0,0,0,0,0,1,0,
+	0,1,0,0,0,0,0,0,1,0,
 	0,0,1,0,0,0,0,1,0,0,
+	0,0,1,0,0,0,0,1,0,0,
+	0,0,1,0,0,0,0,1,0,0,
+	0,0,0,1,1,1,1,0,0,0,
+	0,0,0,1,1,1,1,0,0,0,
 	0,0,0,1,1,1,1,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0
     };
 
     std::vector<unsigned int> circleImageVector(circleImage, circleImage + sizeof(circleImage) / sizeof(circleImage[0]));
     Image RedCircle = Image();
     RedCircle.SetImage(circleImageVector, width, height);
 
+    unsigned int outlineImage[] =
+    {
+        1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,1,0,0,0,0,1,0,1,
+        1,0,1,0,0,0,0,1,0,1,
+        1,0,1,0,1,1,0,1,0,1,
+        1,0,1,0,1,1,0,1,0,1,
+        1,0,1,0,1,1,0,1,0,1,
+        1,0,1,0,1,1,0,1,0,1,
+        1,0,1,0,0,0,0,1,0,1,
+        1,0,1,0,0,0,0,1,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1
+    };
+
+    std::vector<unsigned int> outlineImageVector(outlineImage, outlineImage + sizeof(outlineImage) / sizeof(outlineImage[0]));
+    Image OutlineImage = Image();
+    OutlineImage.SetImage(outlineImageVector, width, height);
+
+    unsigned int crossMarkImage[] =
+    {
+        1,1,0,0,0,0,0,0,1,1,
+        1,1,0,0,0,0,0,0,1,1,
+        1,1,0,0,0,0,0,0,1,1,
+        0,1,1,0,0,0,0,1,1,0,
+        0,1,1,0,0,0,0,1,1,0,
+        0,0,1,1,0,0,1,1,0,0,
+        0,0,1,1,0,0,1,1,0,0,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,0,1,1,0,0,0,0,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,1,1,0,0,1,1,0,0,
+        0,0,1,1,0,0,1,1,0,0,
+        0,1,1,0,0,0,0,1,1,0,
+        0,1,1,0,0,0,0,1,1,0,
+        1,1,0,0,0,0,0,0,1,1,
+        1,1,0,0,0,0,0,0,1,1,
+        1,1,0,0,0,0,0,0,1,1
+    };
+
+    std::vecotor<unsigned int> crossMarkImageVector(crossMarkImage, crossMarkImage + sizeof(crossMarkImage) / sizeof(crossMarkImage[0]));
+    Image CrossMarkImage = Image();
+    CrossMarkImage.SetImage(crossMarkImageVector, width, height);
+
     colorMap.insert(std::pair<Colors, Image>(Blue, BlueSquare));
     colorMap.insert(std::pair<Colors, Image>(Red, RedCircle));
+    colorMap.insert(std::pair<Colors, Image>(Green, OutlineImage));
+    colorMap.insert(std::pair<Colors, Image>(Yellow, CrossMarkImage));
 
     return colorMap;
 }
